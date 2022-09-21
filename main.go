5a7b322909831932aa0319a049959667e6ddfde4
muhammad ian rush <muhammadianrush2@gmail.com>
2022-09-21 11:41:00 +0700

    menambahkan if condition pada file main.go

--- main.go
+++ main.go
@@ -7,4 +7,13 @@ func main() {
 	fmt.Println("hallo dunia")
 	fmt.Println("hallo indonesia")
 	fmt.Println("i'm from medan")
+
+	var number = 5
+	if number == 5 {
+		fmt.Println("true")
+	} else if number == 3 {
+		fmt.Println("false")
+	} else {
+		fmt.Println("try again")
+	}
 }
